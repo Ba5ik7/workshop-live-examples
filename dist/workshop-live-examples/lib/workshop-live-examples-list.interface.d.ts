8498e5bee4e4b4ec4b72fd5d43e6e57b7cabe36d
Wesley DuSell <dusellfam@gmail.com>
2022-06-05 11:54:45 -0400

    Testing an example component.

--- dist/workshop-live-examples/lib/workshop-live-examples-list.interface.d.ts
+++ dist/workshop-live-examples/lib/workshop-live-examples-list.interface.d.ts
@@ -1,17 +1,33 @@
 export interface WorkshopExampleList {
 }
+export interface LiveExample {
+    /** Title of the example. */
+    title: string;
+    /** Name of the example component. */
+    componentName: string;
+    /** Selector to match the component of this example. */
+    selector: string;
+    /** Name of the primary file of this example. */
+    primaryFile: string;
+    /** List of files which are part of the example. */
+    files: string[];
+    /** Path to the directory containing the example. */
+    packagePath: string;
+    /** List of additional components which are part of the example. */
+    additionalComponents: string[];
+    /** NgModule that declares this example. */
+    module: NgModuleInfo;
+}
+export interface NgModuleInfo {
+    /** Name of the NgModule. */
+    name: string;
+    /**
+     * Import specifier that resolves to this module. The specifier is not scoped to
+     * `@angular/components-examples` because it's up to the consumer how the module is
+     * imported. For example, in the docs app, modules are lazily imported from `fesm2015/`.
+     */
+    importSpecifier: string;
+}
 export declare const EXAMPLE_COMPONENTS: {
-    "workshop-live-examples": {
-        packagePath: string;
-        title: string;
-        componentName: string;
-        files: string[];
-        selector: string;
-        additionalComponents: never[];
-        primaryFile: string;
-        module: {
-            name: string;
-            importSpecifier: string;
-        };
-    };
+    [id: string]: LiveExample;
 };
